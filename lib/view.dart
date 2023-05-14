ef9e0b6fe3eff9ed6109f2d9c50e2cea349fe176
Masrafi <masrafianam@gmail.com>
2023-05-14 17:15:48 +0600

    new add

--- lib/view.dart
+++ lib/view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weatherinterface/model.dart';
-import 'mywrapper.dart';
+import 'mypresentation.dart';
 
 class View extends StatefulWidget {
   @override
@@ -8,13 +7,7 @@ class View extends StatefulWidget {
 }
 
 class _ViewState extends State<View> {
-  late MyWrapper mywrapper;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    mywrapper = MyWrapper();
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -28,28 +21,8 @@ class _ViewState extends State<View> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(
-          child: FutureBuilder<WeatherModel>(
-            future: mywrapper.doSomething(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Spacer(),
-                    Text('temp: ${snapshot.data!.temp.toString()}'),
-                    Text('tempMax: ${snapshot.data!.tempMax.toString()}'),
-                    Text('tempMin: ${snapshot.data!.tempMin.toString()}'),
-                    Spacer(),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              // By default, show a loading spinner
-              return const CircularProgressIndicator();
-            },
-          ),
+        child: const Center(
+          child: MyPresentation(),
         ),
       ),
     );
